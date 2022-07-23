49474c4d4078a8c0ada311393cf62c48d12a73a5
Gilmar Oliveira Queiroz <enggilmarqueiroz@gmail.com>
2022-07-23 01:45:58 -0300

    ChatFlutter

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        iconTheme: IconThemeData(
+        iconTheme: const IconThemeData(
           color: Colors.blue,
         )
       ),
